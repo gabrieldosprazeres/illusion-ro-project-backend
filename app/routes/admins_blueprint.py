4b69b05d1c63659ab261934e1b14a4a59ad41483
Gabriel dos Prazeres <gabrieldosprazeres@hotmail.com.br>
2021-12-23 14:35:44 -0300

    feat: adding get_lead_by_email route

--- app/routes/admins_blueprint.py
+++ app/routes/admins_blueprint.py
@@ -1,6 +1,9 @@
 from flask import Blueprint
-from app.controllers.admins_controller import create_admin
+from app.controllers.admins_controller import create_admin, get_lead_by_email
+from flask_jwt_extended import jwt_required
+
 
 bp_admins = Blueprint('bp_admins', __name__, url_prefix='/admin')
 
+bp_admins.get('/lead/<string:email>')(jwt_required()(get_lead_by_email))
 bp_admins.post('')(create_admin)
