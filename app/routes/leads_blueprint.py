2bbad68ef726ca8f4914bb7bf15f487066674402
Gabriel dos Prazeres <gabrieldosprazeres@hotmail.com.br>
2021-12-27 17:11:55 -0300

    fix: adjusting get_leads route

--- app/routes/leads_blueprint.py
+++ app/routes/leads_blueprint.py
@@ -6,6 +6,6 @@ from flask_jwt_extended import jwt_required
 bp_leads = Blueprint('bp_leads', __name__, url_prefix='/leads')
 
 
-bp_leads.get('')(jwt_required()(get_leads))
+bp_leads.get('')(get_leads)
 bp_leads.post('')(create_lead)
 bp_leads.delete('/<int:lead_id>')(delete_lead)
